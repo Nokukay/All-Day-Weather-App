43b645160c8ca9c67fb8dd3d971beff69aeb1808
Nokukay <139324101+Nokukay@users.noreply.github.com>
2024-08-31 20:38:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,3 +53,10 @@ function search(event) {
 
   axios.get(apiUrl).then(displayTemperature);
 }
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
