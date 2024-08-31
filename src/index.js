42fa1fded2f89d42573c090c696562ac46028936
Nokukay <139324101+Nokukay@users.noreply.github.com>
2024-08-31 20:30:57 +0200

    Added FormatDate function

--- src/index.js
+++ src/index.js
@@ -8,3 +8,30 @@ function darkMode() {
 }
 let themebutton = document.querySelector("button");
 themebutton.addEventListener("click", darkMode);
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
